7f5e3823b91437f7c347172a5f789c8670d6e4a1
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-17 01:22:51 -0400

    Apollo installed, chasing expressMiddleware error

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-export default mongoose.connection;
+const db = async(): Promise<typeof mongoose.connection> => {
+  try {
+    if(!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI is not defined! Please check your environment variables.');
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to Mongo Database Successfully.');
+    return mongoose.connection;
+  } catch (error) {
+    console.error('Database connection error:', error);
+    throw new Error('Database connection failed.');
+  }
+};
+
+export default db;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,25 +2,47 @@ import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+
 import db from './config/connection.js';
 import routes from './routes/index.js';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const startApolloServer = async () => {
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  await server.start();
+  await db();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use('/graphql', expressMiddleware(server));
+  
+  app.use(routes);
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+    app.get('*', (_req, res) => {
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
+  }
+
+
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}!`);
+    console.log(`GraphQL available at http://localhost:${PORT}/graphql`);
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();

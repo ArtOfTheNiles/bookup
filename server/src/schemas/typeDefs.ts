41b97ab7cb5e68aaed9152a446121fcbcb6a4f7c
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-23 18:22:22 -0400

    Mutations added for user login and books, bookInput created

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -17,6 +17,14 @@ export const typeDefs = gql`
     link: String
     title: String!
   }
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
   type Query {
     me: User
@@ -27,11 +35,15 @@ export const typeDefs = gql`
   }
 
   type Mutation {
-
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
   }
-  
+
   type Auth {
-    
+    token: String!
+    user: User
   }
 `;
 

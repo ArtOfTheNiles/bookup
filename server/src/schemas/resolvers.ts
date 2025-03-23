1e0987fb689fef4a9b8301ac011543ba5ec8d203
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-23 18:20:41 -0400

    User authentication resolver added

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,10 +1,18 @@
-export const resolvers = {
+import User from '../models/User.js';
+import Book from '../models/Book.js';
+
+export const resolvers: any = {
   Query: {
-    
+    me: async (_:unknown, __:unknown, context: { user?: {_id: string}}) => {
+      if(context.user && context.user._id) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new Error('Not authenticated');
   },
   Mutation: {
 
   },
 }
+};
 
 export default resolvers;

86f0f6f6a1c632a8a082da3a9600eceb41c194ed
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-23 18:38:50 -0400

    Remove Book resolver added and user resolvers scaffolded

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -8,10 +8,48 @@ export const resolvers: any = {
         return User.findOne({ _id: context.user._id });
       }
       throw new Error('Not authenticated');
+    },
+    user: async(_:unknown, userId: { userId: string }) => {
+      return User.findOne({ _id: userId.userId });
+    }
   },
   Mutation: {
-
-  },
+    login: async (_:unknown, { email, password }: { email: string, password: string }) => {
+      
+    },
+    addUser: async (_:unknown, { username, email, password }: { username: string, email: string, password: string }) => {
+      
+    },
+    saveBook: async (_:unknown, bookData: typeof Book, context: { user?: {_id: string}}) => {
+      if (!context.user) {
+        throw new Error('You must be logged in to complete this action');
+      }
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }catch (error) {
+        throw new Error('Error saving book: ' + error);
+      }
+    },
+    removeBook: async (_:unknown, bookId: string, context: { user?: {_id: string}}) => {
+      if (!context.user) {
+        throw new Error('You must be logged in to complete this action');
+      }
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }catch (error) {
+        throw new Error('Error removing book: ' + error);
+      }
+  }
 }
 };
 

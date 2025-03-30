572c9715e26287c096c29f88f457818d8e137080
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-30 15:04:52 -0400

    GraphQL tokens and wrapping my head around them

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,15 +1,16 @@
 import type { User } from '../models/User.js';
 import type { Book } from '../models/Book.js';
 
+// NO NEED in GraphQL API ❌
 // route to get logged in user's info (needs the token)
-export const getMe = (token: string) => {
-  return fetch('/api/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
+// export const getMe = (token: string) => {
+//   return fetch('/api/users/me', {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       authorization: `Bearer ${token}`,
+//     },
+//   });
+// };
 
 export const createUser = (userData: User) => {
   return fetch('/api/users', {
